3ec55754288da279e079646523f2de2eeb166cd5
kriegsmaschine <scukras7@gmail.com>
2018-03-11 17:57:50 -0400

    initial commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,16 @@
+var express       = require('express');
+var router        = express.Router();
+
+//import controllers
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers    = require('../controllers/others');
+
+//Locations pages
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+//Others pages
+router.get('/about', ctrlOthers.about);
+
+module.exports = router;
